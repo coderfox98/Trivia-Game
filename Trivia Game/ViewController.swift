17e244b5b6e8a8d84955cb41e72bf9db0eaf0c47
rish <coderfox98@gmail.com>
2018-11-20 01:04:53 +0530

    Initial Commit

---
+++ Trivia Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivia Game
+//
+//  Created by Rish on 20/11/18.
+//  Copyright © 2018 Rish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
